eed6bc8b847f2beaaf4eb76c39d5323cc1d487d9
dimitrich212003 <ytrewq212003@mail.ru>
2023-12-13 16:26:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import calculator.DeterminantCalculator;
+import calculator.impl.Calculator;
+import calculator.impl.ThreadedCalculator;
+
+import java.util.Random;
+
+public class Main {
+    private static final double[][] matrix = generateRandomMatrix(11, 11);
+
+    public static double[][] generateRandomMatrix(int rows, int cols) {
+        Random random = new Random();
+        double[][] matrix = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = random.nextDouble();
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        DeterminantCalculator threadedCalculator = new ThreadedCalculator(matrix, 11);
+        DeterminantCalculator calculator = new Calculator(matrix);
+
+        long startTimeThreaded = System.currentTimeMillis();
+        System.out.println("Результат рекурсивного параллелизма: " + threadedCalculator.calculateDeterminant());
+        long endTimeThreaded = System.currentTimeMillis();
+        System.out.printf("%s миллисекунд\n", endTimeThreaded - startTimeThreaded);
+
+
+        long startTime = System.currentTimeMillis();
+        System.out.println("Результат однопоточного алгоритма: " + calculator.calculateDeterminant());
+        long endTime = System.currentTimeMillis();
+        System.out.printf("%s миллисекунд\n", endTime - startTime);
+
+        System.out.println("Для проверки заменим первую строку на 0.0 det(M) = 0");
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[0][i] = 0;
+        }
+
+        DeterminantCalculator testThreadedCalculator = new ThreadedCalculator(matrix, 11);
+        DeterminantCalculator testCalculator = new Calculator(matrix);
+        System.out.printf("Паралельный [%s] = Однопоточный [%s]",
+                testThreadedCalculator.calculateDeterminant(),
+                testCalculator.calculateDeterminant());
+    }
+}
